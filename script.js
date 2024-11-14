76884b025150e85c29f44f9dc0ba12914482df5e
22DP2EKoro <131154546+22DP2EKoro@users.noreply.github.com>
2024-11-14 14:29:28 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,51 @@
+var screen = document.querySelector('#screen');
+var btn = document.querySelectorAll('.btn');
+let historyContent = document.getElementById('history-content');
+
+function calculation(v) {
+    let result;
+    try {
+        result = eval(v); 
+    } catch (e) {
+        result = "Error";
+    }
+
+    
+    let para = document.createElement('p');
+    para.innerHTML = v + " = " + result;
+
+    
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'Delete';
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.addEventListener('click', () => {
+        para.remove(); 
+    });
+
+    
+    para.appendChild(deleteBtn);
+
+
+    historyContent.appendChild(para);
+
+    return result;
+}
+
+for (item of btn) {
+    item.addEventListener('click', (e) => {
+        let btntext = e.target.innerText;
+        screen.value += btntext;
+    });
+}
+
+function backspc() {
+    screen.value = screen.value.substr(0, screen.value.length - 1);
+}
+
+function clearScreen() {
+    screen.value = '';
+}
+
+function clearHistory() {
+    historyContent.innerHTML = ''; 
+}
